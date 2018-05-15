9ab14d58e47fc71335b4e53fb08d785e213f86e7
Jesús <jinacios@jaumebalmes.net>
2018-05-15 16:57:29 +0200

    Hay que ponerle la id en esa funcion, sino no lo devolvia bien

--- app/User.php
+++ app/User.php
@@ -30,7 +30,7 @@ class User extends Authenticatable
      */
     public function role()
     {
-        return $this->belongsTo(Role::class);
+        return $this->belongsTo(Role::class, 'id');
     }
 
     /**
