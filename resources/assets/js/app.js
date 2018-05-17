d0dfaa8738f0497fe090019782f8663dca99ad03
Jesús <jinacios@jaumebalmes.net>
2018-05-17 17:04:29 +0200

    Revert "PreLogin"
    This reverts commit 3f870375ed2d26e2abab2e140cdf831f58848fba.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,8 +3,7 @@ require('./bootstrap');
 window.axios = require('axios');
 
 window.Vue = require('vue');
-Vue.component('login', require('./components/LoginComponent.vue'));
-Vue.component('register', require('./components/Register.vue'));
+
 const app = new Vue({
     el: '#app',
     created: function(){
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,3 @@
 Route::get('/', function () {
     return view('app');
 });
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
