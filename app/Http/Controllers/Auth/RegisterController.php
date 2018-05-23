95f5ebfa1f10b9b81702f234804e6159edb3eadc
AGonzalezLC <agonzale@jaumebalmes.net>
2018-05-23 23:06:04 +0200

    Añadimos atributos al create del usuario
    Añadimos los campos que necesitamos para el registro en el metodo para crear los usuarios por laravel y su correspondiente validador.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,9 +49,14 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
+            'name'             =>   'required|string|max:255',
+            'email'            =>   'required|string|email|max:255|unique:users',
+            'password'         =>   'required|string|min:6|confirmed',
+            'rol_id'           =>   'required',
+            'provincia_id'     =>   'required',
+            'telefono'         =>   'required',
+            'direccion'        =>   'required',
+            'codigo_postal'    =>   'required'
         ]);
     }
 
@@ -64,9 +69,16 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'name'           =>     $data['name'],
+            'email'          =>     $data['email'],
+            'password'       =>     Hash::make($data['password']),
+            'rol_id'         =>     $data['rol_id'],
+            'provincia_id'   =>     $data['provincia_id'],
+            'telefono'       =>     $data['telefono'],
+            'direccion'      =>     $data['direccion'],
+            'codigo_postal'  =>     $data['codigo_postal'],
+            'rango_desde'    =>     0,
+            'rango_hasta'    =>     1000000
         ]);
     }
 }
