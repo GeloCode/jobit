79ce691ae7398fa259ca4c7ed5feeecc42186dd5
AGonzalezLC <agonzale@jaumebalmes.net>
2018-05-30 19:52:24 +0200

    ProfileBack

--- Database_Data/roles.sql
+++ Database_Data/roles.sql
@@ -1,3 +1,3 @@
-INSERT INTO roles VALUES (1, 'Empresa', '2018-05-15 00:00:00', '2018-05-15 00:00:00');
-INSERT INTO roles VALUES (2, 'Solicitante', '2018-05-15 00:00:00', '2018-05-15 00:00:00');
-INSERT INTO roles VALUES (3, 'Administrador', '2018-05-15 00:00:00', '2018-05-15 00:00:00');
+INSERT INTO roles VALUES (1, 'Empresa', 1, '2018-05-15 00:00:00', '2018-05-15 00:00:00');
+INSERT INTO roles VALUES (2, 'Solicitante', 1, '2018-05-15 00:00:00', '2018-05-15 00:00:00');
+INSERT INTO roles VALUES (3, 'Administrador', 1, '2018-05-15 00:00:00', '2018-05-15 00:00:00');
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -10,7 +10,8 @@ Vue.component('detail', require('./components/RootComponent.vue'));
 //Vue.component('detalle', require('./components/Detalleportf.vue'));
 Vue.component('login', require('./components/Login.vue'));
 Vue.component('register', require('./components/Register.vue'));
-Vue.component('oferta', require('./components/OfertaComponent.vue'));
+//Vue.component('oferta', require('./components/OfertaComponent.vue'));
+Vue.component('perfil', require('./components/Perfil.vue'));
 const app = new Vue({
     el: '#app',
 });
